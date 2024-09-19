841b22a859f7b7dcdd5ae7f53dd821973ced7f5f
ChandupaBAndaranayake <cicmurox@gmail.com>
2024-09-19 17:17:02 +0530

    fix: execute.sh
    fix: test.yaml

--- execute.sh
+++ execute.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p out
+chmod 777 out
 
 commit_hash=$(git rev-parse --short HEAD)
 commit_message=$(git log -1 --pretty=%B)
@@ -10,3 +11,5 @@ echo "Commit: $commit_hash" >> out/commits.txt
 echo "Message: $commit_message" >> out/commits.txt
 echo "Time: $commit_time" >> out/commits.txt
 echo "--------------------" >> out/commits.txt
+
+chmod 777 out/commits.txt
