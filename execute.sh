ad00e5de2c137c8eb58b4c5b41b7069d3356c480
ChandupaBAndaranayake <cicmurox@gmail.com>
2024-09-19 15:00:22 +0530

    fix: execute.sh path

--- execute.sh
+++ execute.sh
@@ -6,7 +6,7 @@ commit_hash=$(git rev-parse --short HEAD)
 commit_message=S(git log -1 --pretty=%B)
 commit_time=$(git log -1 --format=%cd)
 
-echo "Commit: $commit_hash" >> out.commits.txt
-echo "Message: $commit_message" >> out.commits.txt
-echo "Time: $commit_time" >> out.commits.txt
-echo "--------------------" >> out.commits.txt
+echo "Commit: $commit_hash" >> out/commits.txt
+echo "Message: $commit_message" >> out/commits.txt
+echo "Time: $commit_time" >> out/commits.txt
+echo "--------------------" >> out/commits.txt
