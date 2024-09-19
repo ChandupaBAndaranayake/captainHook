e85ffb0ea968f26c5316e260bd222fb7a3d524e4
ChandupaBAndaranayake <cicmurox@gmail.com>
2024-09-19 15:16:42 +0530

    fix: execute.sh

--- execute.sh
+++ execute.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "running!"
+
 mkdir -p out
 
 commit_hash=$(git rev-parse --short HEAD)
-commit_message=S(git log -1 --pretty=%B)
+commit_message=$(git log -1 --pretty=%B)
 commit_time=$(git log -1 --format=%cd)
 
 echo "Commit: $commit_hash" >> out/commits.txt
